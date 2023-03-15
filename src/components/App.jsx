3ec470f45b300be24d4be2c62b85d1f9e28537d3
Kirill Tsiutsiura <kirill.tsiutsiura7@gmail.com>
2023-03-15 16:36:25 +0200

    profile task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,6 @@
+import { Profile } from './Profile/Profile';
+import user from '../../src/user.json';
+
 export const App = () => {
   return (
     <div
@@ -7,10 +10,18 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+      />
     </div>
   );
 };
